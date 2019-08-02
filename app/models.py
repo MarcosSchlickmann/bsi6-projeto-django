1ab3dc5818671657caa174fb4c4572e278e26405
MarcosSchlickmann <marcosamichels@gmail.com>
2019-08-02 11:43:39 -0300

    Outros

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.model):
+    title = models.CharField()
+    description = models.CharField()
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/aluno/Documentos/bsi6-projeto-django/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
