6b065ded83a00ab55ef346d6825a1a8fbfa22af9
MarcosSchlickmann <marcosamichels@gmail.com>
2019-08-24 17:38:48 -0300

    Cadastrar dashboards

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard_list, name='dashboard_list'),
+    path('dashboards/', views.dashboard_list, name='dashboard_list'),
+    path('dashboards/new/', views.dashboard_new, name='dashboard_new'),
+    path('dashboards/<int:pk>/', views.dashboard_detail, name='dashboard_detail')
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
+from django.views import View
 from .models import Dashboard
+from .forms import DashboardForm
+
 
 # Create your views here.
 
 def dashboard_list(request):
     dashboards = Dashboard.objects.all()
     return render(request, "app/dashboard_list.html", {'dashboards': dashboards})
+
+
+def dashboard_detail(request, pk):
+	dashboard = get_object_or_404(Dashboard, pk=pk)
+	return render(request, 'app/dashboard_detail.html', {'dashboard': dashboard})
+
+
+def dashboard_new(request):
+	if request.method == 'POST':
+		form = DashboardForm(request.POST)
+		if form.is_valid():
+			dashboard = form.save()
+			return redirect('dashboard_detail', pk=dashboard.pk)
+	else:
+		form = DashboardForm()
+	return render(request, 'app/dashboard_edit.html', {'form': form})
